efd5759d8ccf107861a3430b60827852bd75a4cd
Matheus Viscki <matheusviscki@gmail.com>
2024-06-10 19:42:25 -0300

    feat: Add Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import { Button } from '@mui/material'
+import '@fontsource/roboto' // Importa a fonte Roboto
+
 export function App() {
   return (
     <>
       <h1>My App</h1>
-      <p>Welcome to my app!</p>
+      <p style={{ fontFamily: 'Roboto' }}>Welcome to my app!</p>
+      <Button variant="text">Click me!</Button>
     </>
   )
 }
